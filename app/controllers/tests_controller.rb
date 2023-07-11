56faf1879ef246288795fb7facfecd9233541892
enoch <enokkang@gmail.com>
2023-07-11 15:16:23 -0500

    adds socket codes.

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,2 +1,5 @@
 class TestsController < ApplicationController
+  def index
+    render json: { message: "hello" }
+  end
 end
---
+++ test.rb
@@ -0,0 +1,15 @@
+require "socket"
+
+host = "10.0.0.184"     # The web server
+port = 3000                           # Default HTTP port
+path = "/one_product.json"                 # The file we want
+
+# This is the HTTP request we send to fetch a file
+request = "GET #{path} HTTP/1.0\r\n\r\n"
+
+socket = TCPSocket.open(host, port)  # Connect to server
+socket.print(request)               # Send request
+response = socket.read              # Read complete response
+# Split response at first blank line into headers and body
+headers, body = response.split("\r\n\r\n", 2)
+print body                          # And display it
